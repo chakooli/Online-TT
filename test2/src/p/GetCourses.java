dcd90cfdf9c59220a3d9a0ad445ccb4b01f5bce2
Parinita <parihoomai@gmail.com>
2015-02-16 20:02:16 +0530

    modified

--- test2/src/p/GetCourses.java
+++ test2/src/p/GetCourses.java
@@ -4,13 +4,13 @@ import java.sql.*;
 public class GetCourses {
 		Dbcon databasecon = new Dbcon();
 		Connection con = databasecon.dbconnect();
-		public int courses() throws SQLException{
+		public long courses(String username) throws SQLException{
 			int i=0, count = 0;
 			Getup g = new Getup();
-			int u = g.getUsername();
+			int u = Integer.parseInt(username);
 			PreparedStatement ps= con.prepareStatement("select " +
 					"count(*) from reg_courses where stud_rollno = ?");
-			ps.setInt(1, u);
+			ps.setLong(1, u);
 			ResultSet rs = ps.executeQuery();
 			while(rs.next()){
 				count = rs.getInt(1);
@@ -18,7 +18,7 @@ public class GetCourses {
 			String regcourses[] = new String[count];
 			ps = con.prepareStatement("select coursecode" +
 					" from reg_courses where stud_rollno = ?");
-			ps.setInt(1, u);
+			ps.setLong(1, u);
 			rs = ps.executeQuery();
             while(rs.next()){
                 regcourses[i] = rs.getString("coursecode");
